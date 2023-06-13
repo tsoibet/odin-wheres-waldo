54cb8f2776f8b28ceb2b61d0a457e474d01d861f
Betsy Tsoi <tsoisybetsy@gmail.com>
2023-06-13 23:48:26 +0900

    Implement function to retrive data from firestore

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,12 +1,26 @@
+import { firebaseConfig } from "./firebaseConfig";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore"; 
+import { useEffect, useState } from "react";
+
 export default function Leaderboard(props) {
 
-  const records = [{
-    name: "Bet",
-    score: 9
-  }, {
-    name: "Tsoi",
-    score: 1
-  }];
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    async function getRecords() {
+      try {
+        const querySnapshot = await getDocs(collection(db, "records"));
+        const temp = querySnapshot.docs.map((record) => record.data());
+        setRecords(temp);
+      } catch (error) {
+        console.error("Error retrieving data: ", error);
+      }
+    };
+    getRecords();
+  }, []);
 
   const start = props.start;
 
@@ -19,13 +33,12 @@ export default function Leaderboard(props) {
       <ol>
         {
           records.map((record, index) => 
-            <Record key={index} rank={index + 1} name={record.name} score={record.score} />)
+            <Record key={index} rank={index + 1} name={record.name} score={record.score} timestamp={record.timestamp} />)
         }
       </ol>
       <button onClick={start}>
         Start
       </button>
-
     </div>
   );
 };
@@ -35,6 +48,7 @@ function Record(props) {
   const rank = props.rank;
   const name = props.name;
   const score = props.score;
+  const timestamp = props.timestamp.toDate().toLocaleDateString();
 
   return(
     <li className="Record">
@@ -46,6 +60,9 @@ function Record(props) {
       </span>
       <span>
         ({score})
+      </span>
+      <span>
+        ({timestamp})
       </span>
     </li>
   );
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCTmfMkCtCZaBDkb84PsFzDq8dgFWOAHIk",
+  authDomain: "odin-where-s-waldo-9fa41.firebaseapp.com",
+  projectId: "odin-where-s-waldo-9fa41",
+  storageBucket: "odin-where-s-waldo-9fa41.appspot.com",
+  messagingSenderId: "527415961752",
+  appId: "1:527415961752:web:802c9cb4ba2ca873e708ed",
+  measurementId: "G-3P6CHNC16M"
+};
