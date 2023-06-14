b854c4ca3cd1aa356b0ede24f30cc284be7ada91
Betsy Tsoi <tsoisybetsy@gmail.com>
2023-06-14 23:33:58 +0900

    Sort and limit records to display on leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,6 +1,6 @@
 import { firebaseConfig } from "./firebaseConfig";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore"; 
+import { getFirestore, query, collection, orderBy, limit, getDocs } from "firebase/firestore"; 
 import { useEffect, useState } from "react";
 
 export default function Leaderboard(props) {
@@ -8,11 +8,12 @@ export default function Leaderboard(props) {
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
   const [records, setRecords] = useState([]);
-
+  
   useEffect(() => {
     async function getRecords() {
       try {
-        const querySnapshot = await getDocs(collection(db, "records"));
+        const q = query(collection(db, "records"), orderBy("score"), limit(10) )
+        const querySnapshot = await getDocs(q);
         const temp = querySnapshot.docs.map((record) => record.data());
         setRecords(temp);
       } catch (error) {
@@ -48,18 +49,30 @@ function Record(props) {
   const rank = props.rank;
   const name = props.name;
   const score = props.score;
-  const timestamp = props.timestamp.toDate().toLocaleDateString();
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  const timestamp = props.timestamp.toDate().toLocaleDateString("en-US", options);
+
+  const ms = (Math.floor(score / 10) % 100).toString().padStart(2, '0');
+  const seconds = (Math.floor(score / 1000) % 60).toString().padStart(2, '0');
+  const minutes = (Math.floor(score / 60000) % 60).toString().padStart(2, '0');
+  const hours = (Math.floor(score / 3600000)).toString().padStart(2, '0');
 
   return(
     <li className="Record">
-      {/* <span>
-        {rank}:
-      </span> */}
       <span>
+        {rank}
+      </span>
+      <span className="name">
         {name}
       </span>
       <span>
-        ({score})
+        { hours > 0 && <span> <span className="number"> {hours}</span>h </span> }
+        { minutes > 0 && <span> <span className="number"> {minutes}</span>m </span> }
+        <span> <span className="number"> {seconds}.{ms}</span>s </span>
       </span>
       <span>
         ({timestamp})
